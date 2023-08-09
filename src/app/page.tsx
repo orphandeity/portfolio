4ffa2f17892bfeb07d592a49b570105c497aed3c
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-08-08 23:12:20 -0700

    project cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,104 @@
+import Projects from '@/components/projects'
+import Css from '@/components/svg/css'
+import Express from '@/components/svg/express'
+import Figma from '@/components/svg/figma'
+import Git from '@/components/svg/git'
+import Github from '@/components/svg/github'
+import Html from '@/components/svg/html'
+import Javascript from '@/components/svg/javascript'
+import Mysql from '@/components/svg/mysql'
+import Next from '@/components/svg/next'
+import Node from '@/components/svg/node'
+import Postgres from '@/components/svg/postgres'
+import React from '@/components/svg/react'
+import Sass from '@/components/svg/sass'
+import Svelte from '@/components/svg/svelte'
+import Typescript from '@/components/svg/typescript'
 import { ContactForm } from '@/components/ui/contact-form'
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
-      <h1>Hi there! I&apos;m Jeff. Web Developer, Musician and Cat Lover.</h1>
-      <p className="text-xl text-muted-foreground">
-        I build dynamic web experiences that people love to use. You can read a
-        bit more{' '}
-        <Link
-          href={'about-me'}
-          className="decoration-42 underline decoration-primary underline-offset-4"
-        >
-          about me
-        </Link>{' '}
-        or check out some things I&apos;ve made.
-      </p>
-      <h2>Skills</h2>
-      <h3>Languages</h3>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>Sass</li>
-        <li>JavaScript</li>
-        <li>TypeScript</li>
-      </ul>
-      <h3>Frameworks</h3>
-      <ul>
-        <li>React</li>
-        <li>Next</li>
-        <li>Svelte</li>
-        <li>Express</li>
-        <li>Node</li>
-      </ul>
-      <h3>Tools</h3>
-      <ul>
-        <li>Git</li>
-        <li>GitHub</li>
-        <li>MySQL</li>
-        <li>PostgreSQL</li>
-        <li>Figma</li>
-      </ul>
-      <h2>Projects</h2>
-      <h3>Entertainment Web App</h3>
-      <h3>Galleria Slideshow Site</h3>
-      <h3>Planets Fact Site</h3>
-      <h2>Contact</h2>
-      <ContactForm />
+      <div>
+        <h1>Hi there! I&apos;m Jeff. Web Developer, Musician and Cat Lover.</h1>
+        <p className="text-xl text-muted-foreground">
+          I build dynamic web experiences that people love to use. You can read
+          a bit more{' '}
+          <Link
+            href={'about-me'}
+            className="decoration-42 underline decoration-primary underline-offset-4"
+          >
+            about me
+          </Link>{' '}
+          or check out some things I&apos;ve made.
+        </p>
+      </div>
+
+      <section>
+        <h2>Skills</h2>
+        <h3>Languages</h3>
+        <ul className="flex justify-between">
+          <li className="w-12">
+            <Html />
+          </li>
+          <li className="w-12">
+            <Css />
+          </li>
+          <li className="w-12">
+            <Sass />
+          </li>
+          <li className="w-12">
+            <Javascript />
+          </li>
+          <li className="w-12">
+            <Typescript />
+          </li>
+        </ul>
+        <h3>Frameworks</h3>
+        <ul className="flex justify-between">
+          <li className="w-12">
+            <React />
+          </li>
+          <li className="w-12">
+            <Next />
+          </li>
+          <li className="w-12">
+            <Svelte />
+          </li>
+          <li className="w-12">
+            <Express />
+          </li>
+          <li className="w-12">
+            <Node />
+          </li>
+        </ul>
+        <h3>Tools</h3>
+        <ul className="flex justify-between">
+          <li className="w-12">
+            <Git />
+          </li>
+          <li className="w-12">
+            <Github />
+          </li>
+          <li className="w-12">
+            <Mysql />
+          </li>
+          <li className="w-12">
+            <Postgres />
+          </li>
+          <li className="w-12">
+            <Figma />
+          </li>
+        </ul>
+      </section>
+
+      <Projects />
+
+      <section>
+        <h2>Contact</h2>
+        <ContactForm />
+      </section>
     </main>
   )
 }
---
+++ src/components/projects.tsx
@@ -0,0 +1,101 @@
+import Image from 'next/image'
+import { Badge } from './ui/badge'
+import { Button } from './ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card'
+
+export default function Projects() {
+  let projects = [
+    {
+      name: 'Entertainment Web App',
+      description:
+        'A web app that allows users to search for movies, tv shows, and games. Users can also create an account to save their favorite movies, tv shows, and games.',
+      technologies: ['React', 'Next', 'Typescript', 'Tailwind'],
+      screenshot: '/project_1.png',
+      links: {
+        github: 'https://github.com/orphandeity/entertainment-web-app.git',
+        live: 'https://entertainment-web-app-tan.vercel.app/',
+      },
+    },
+    {
+      name: 'Galleria Slideshow Site',
+      description:
+        'A slideshow site that allows users to upload images and create slideshows.',
+      technologies: ['React', 'Next', 'Typescript', 'Tailwind'],
+      screenshot: '/project_2.png',
+      links: {
+        github: 'https://github.com/orphandeity/galleria-slideshow-site.git',
+        live: 'https://galleria-slideshow-site-amber.vercel.app/',
+      },
+    },
+    {
+      name: 'Planets Fact Site',
+      description:
+        'A site that displays facts about the planets in our solar system.',
+      technologies: ['React', 'Next', 'Typescript', 'Tailwind'],
+      screenshot: '/project_3.png',
+      links: {
+        github: 'https://github.com/orphandeity/planets-fact-site.git',
+        live: 'https://planets-fact-site-two.vercel.app/',
+      },
+    },
+  ]
+
+  return (
+    <section>
+      <h2>Projects</h2>
+      <ul className="flex flex-wrap gap-8">
+        {projects.map((project) => (
+          <li key={project.name}>
+            <Card className="max-w-md">
+              <CardHeader>
+                <CardTitle>{project.name}</CardTitle>
+                <CardDescription>{project.description}</CardDescription>
+                <ul className="flex flex-wrap gap-2">
+                  {project.technologies.map((technology) => (
+                    <li key={technology}>
+                      <Badge variant={'secondary'}>{technology}</Badge>
+                    </li>
+                  ))}
+                </ul>
+              </CardHeader>
+
+              <CardContent>
+                <Image
+                  src={project.screenshot}
+                  width={1440}
+                  height={886}
+                  alt=""
+                />
+              </CardContent>
+              <CardFooter>
+                <ul className="flex">
+                  <li>
+                    <Button variant={'link'} asChild>
+                      <a href={project.links.github} target="_blank">
+                        View code
+                      </a>
+                    </Button>
+                  </li>
+                  <li>
+                    <Button variant={'link'} asChild>
+                      <a href={project.links.live} target="_blank">
+                        View demo
+                      </a>
+                    </Button>
+                  </li>
+                </ul>
+              </CardFooter>
+            </Card>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
