ccd0dc99ba4f858746ede96d66ec19578156e670
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-08-15 22:44:46 -0700

    skills animation

--- src/components/skills-list.tsx
+++ src/components/skills-list.tsx
@@ -1,11 +1,23 @@
+'use client'
+
+import { animate, useInView } from 'framer-motion'
+import { useEffect, useRef } from 'react'
+
 interface SkillsListProps {
   heading: string
   children: React.ReactNode
 }
 
 export default function SkillsList({ heading, children }: SkillsListProps) {
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
+
+  useEffect(() => {
+    animate(ref.current, { opacity: [0, 1] }, { duration: 0.9 })
+  }, [isInView])
+
   return (
-    <div className="lg:space-y-8 lg:text-center">
+    <div ref={ref} className="lg:space-y-8 lg:text-center">
       <h3>{heading}</h3>
       <ul className="flex justify-between lg:mx-auto lg:w-60 lg:flex-wrap lg:justify-center lg:gap-16">
         {children}
