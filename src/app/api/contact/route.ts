87ce3bd94ce5b983143d87ed72f2c04dfd5c2a43
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-10-14 23:28:53 -0700

    updates nodemailer logic

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -6,41 +6,38 @@ export async function POST(request: NextRequest) {
 
   let name = String(body.name)
   let email = String(body.email)
-  let message = String(body.message)
+  let content = String(body.message)
 
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    service: 'gmail',
     auth: {
-      type: 'OAuth2',
       user: process.env.GOOGLE_USER_EMAIL,
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-      accessToken: process.env.GOOGLE_ACCESS_TOKEN,
+      pass: process.env.GOOGLE_PASSWORD,
     },
   })
 
-  try {
-    const mail = await transporter.sendMail({
-      from: process.env.GOOGLE_USER_EMAIL,
-      to: 'jrw1982@mac.com',
-      replyTo: email,
-      subject: `Contact form submission from ${name}`,
-      html: `
-        <p>You have a new contact form submission!</p>
-        <p><strong>Name: </strong> ${name}</p>
-        <p><strong>Email: </strong> ${email}</p>
-        <p><strong>Message: </strong> ${message}</p>
-      `,
-    })
-
-    console.log('Message sent: %s', mail.messageId)
-    return NextResponse.json({ message: 'Message successfully sent!' })
-  } catch (err) {
-    console.log(err)
+  let message = {
+    from: name,
+    to: 'jrw1982@mac.com',
+    subject: `Portfolio Website Message from ${name}`,
+    html: `
+      <h1>⭐️ New Message from Portfolio Website</h1>
+      <p><strong>Name: </strong> ${name}</p>
+      <p><strong>Email: </strong> ${email}</p>
+      <p><strong>Message: </strong> ${content}</p>
+    `,
   }
+
+  transporter.sendMail(message, (error, info) => {
+    if (error) {
+      NextResponse.json({ error })
+    } else {
+      NextResponse.json({
+        message: 'Email sent successfully',
+        info: info.messageId,
+      })
+    }
+  })
 
   return NextResponse.json({ message: 'Something went wrong.' })
 }
